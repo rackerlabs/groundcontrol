a09f2b35ac80313b7d128e407b0ee5c7cf48f30f
Michael Gundlach <gundlach@gmail.com>
2010-05-17 16:43:17 -0400

    Server update and create supported in UI.
    I feel a bit like I'm reinventing the wheel here, but it may just be
    because I've built a few too many CRUD screens in Javascript in my life.
    
    I looked into ExtJS but it felt really heavy for our needs.  I think it
    will be best to keep the design really simple for now, and if we can't
    jazz up the UI enough with CSS we'll look into using ExtJS or some other
    UI framework.

---
+++ chromium/common/html/paginatedtable.js
@@ -0,0 +1,84 @@
+function PaginatedTable(opts) {
+  var that = this;
+
+  that._manager = opts.manager;
+  that._table = opts.table;
+  that._createRow = opts.createRow;
+  that._currentPage = 0;
+
+  if (opts.rowClick) {
+    that._table.find("tr.dataRow").live("click", function() {
+      var theRow = $(this);
+      opts.rowClick(theRow.data("entity"));
+    });
+  }
+
+  var numCols = that._table.find("thead tr td").length;
+  that._more = $('<tr><td></td></tr>');
+  that._more.find("td").
+    attr("colspan", numCols).
+    append('<a href="#">More...</a>').
+    append('<span class="loading">Loading...</span>');
+  that._more.
+    find(".loading").hide().end().
+    find("a").click(function() { that.loadPage(); }).end();
+
+  that._table.find("tbody").append(this._more);
+}
+
+PaginatedTable.prototype = {
+
+  // Append a page of entities to the list.
+  loadPage: function() {
+    this._more.
+      find("a").hide().end().
+      find(".loading").show();
+
+    var PAGE_SIZE = 10;
+    var offset = this._currentPage * PAGE_SIZE;
+
+    var that = this;
+    that._manager.createList(true, offset, PAGE_SIZE).forEachAsync({
+      each: function(entity) {
+        that.addRowFor(entity);
+      },
+      complete: function(count) {
+        that._currentPage += 1;
+
+        if (count == PAGE_SIZE) { // didn't reach end of list
+          that._more.
+            find(".loading").hide().end().
+            find("a").show();
+        }
+        else {
+          that._more.hide();
+        }
+      }
+    });
+  },
+
+  _makeDataRow: function(entity) {
+    var result = this._createRow(entity);
+    result.addClass("dataRow").data("entity", entity);
+    return result;
+  },
+
+  addRowFor: function(entity) {
+    this._more.before(this._makeDataRow(entity));
+  },
+
+  // Return a jQuery ojbect containing the row(s) for the given entity
+  rowFor: function(entity) {
+    return this._table.
+      find("tr.dataRow").
+      filter(function() {
+        return $(this).data("entity") == entity;
+      });
+  },
+
+  // Replace the row for oldEntity with one for newEntity.
+  replaceRowFor: function(oldEntity, newEntity) {
+    this.rowFor(oldEntity).replaceWith(this._makeDataRow(newEntity));
+  }
+
+}
