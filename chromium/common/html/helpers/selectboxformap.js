7193157b61bce26dd6e8c788a20cee209b4c9518
Michael Gundlach <gundlach@gmail.com>
2010-06-19 02:24:14 -0400

    Assign servers to ip groups upon create

--- chromium/common/html/helpers/selectboxformap.js
+++ chromium/common/html/helpers/selectboxformap.js
@@ -1,8 +1,10 @@
 // Return a <select> box with <option>s whose ids are the keys to the
 // map and whose values are map[key][textAttribute].  The <option> whose
 // id is selectedKey will be selected.
-function selectBoxForMap(map, textAttribute, selectedKey) {
+function selectBoxForMap(map, textAttribute, selectedKey, includeBlank) {
   var result = $("<select>");
+  if (includeBlank)
+    $("<option>").text("<none>").appendTo(result);
   for (var key in map) {
     $("<option>").
       attr("id", key).
