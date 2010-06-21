c9d104fb22b0e32b35524bd385a4d8e0a9bca3e1
Michael Gundlach <gundlach@gmail.com>
2010-06-21 19:04:07 -0400

    comments

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+# Create deploy files.
+
 cd $(dirname $0)
 
 # Firefox
+# You'll need to serve this .xpi file somewhere with a 
+# mime type of application/x-xpinstall or Firefox won't
+# recognize it.
 (
 cd firefox
 rm -f ../GroundControl.xpi
@@ -8,6 +13,7 @@ zip -r ../GroundControl.xpi *
 )
 
 # Chrome
+# Submit this to the Chrome Extensions Gallery.
 (
 zip -r GroundControl.zip chromium
 )
