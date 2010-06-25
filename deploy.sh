5fde1ec03e6743c7a16a966439926630569f3fc5
Michael Gundlach <gundlach@gmail.com>
2010-06-25 11:51:41 -0400

    Deploy instructions

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 cd $(dirname $0)
 
 # Firefox
-# You'll need to serve this .xpi file somewhere with a mime type of
-# application/x-xpinstall or Firefox won't recognize it.  I haven't figured out
-# how we notify current users about updates.
 (
 cd firefox
 rm -f ../GroundControl.xpi
 zip -r ../GroundControl.xpi *
 )
+# Now run
+#   scp GroundControl.xpi root@groundcontrol.rackspace.com:/var/www/
+# I haven't figured out how we notify current users about updates.
 
 # Chrome
+(
+zip -r GroundControl.zip chromium
+)
 # Log into https://chrome.google.com/extensions/developer/dashboard?hl=en 
 # as groundcontrol@rackspace.com and update the extension with this .zip file.
 # You'll need to have updated the version number in the manifest.json or you'll
 # get a complaint.
-(
-zip -r GroundControl.zip chromium
-)
