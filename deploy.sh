048be63ae4249c301b51132b5c0007a6b7313b3f
Michael Gundlach <gundlach@gmail.com>
2010-12-09 12:52:44 -0500

    Detailed deployment instructions

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-# Create files to deploy GroundControl.
+#!/bin/bash
+#
+# Creates files to deploy GroundControl.
+#
+# See below for details on how to deploy the files.
+#
 
 cd $(dirname $0)
 
@@ -8,15 +13,23 @@ cd firefox
 rm -f ../GroundControl.xpi
 zip -r ../GroundControl.xpi *
 )
-# Now run
+# To deploy the new version to Firefox users: run
 #   scp GroundControl.xpi root@groundcontrol.rackspace.com:/var/www/
-# I haven't figured out how we notify current users about updates.
+#
+# I haven't figured out how we notify current users about updates.  For now,
+# users will have to go re-install when they want to get newer versions.
 
 # Chrome
 (
 zip -r GroundControl.zip chromium
 )
-# Log into https://chrome.google.com/extensions/developer/dashboard?hl=en 
-# as groundcontrol@rackspace.com and update the extension with this .zip file.
-# You'll need to have updated the version number in the manifest.json or you'll
-# get a complaint.
+# To deploy the new version to Chrome users:
+# Log into https://chrome.google.com/extensions/developer/dashboard
+# as groundcontrol@rackspace.com, update the extension with this .zip file,
+# and choose Publish.
+#
+# You'll need to have bumped the version number in chromium/manifest.json or
+# you'll get a complaint.
+#
+# Existing users of the extension will automatically be updated to the
+# latest version within a few days.
